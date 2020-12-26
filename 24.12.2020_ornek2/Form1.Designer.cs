17703f59b139e6cdb91eb6d663c3c6405e2fa808
Tolga Nogay <47275502+TolgaNogay@users.noreply.github.com>
2020-12-26 21:17:26 +0300

    👋 - The file has been successfully added.
    The files were completely uploaded to the internet.

---
+++ 24.12.2020_ornek2/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace _24._12._2020_ornek2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Helvetica Neue", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(52, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İl";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Antalya",
+            "Isparta"});
+            this.comboBox1.Location = new System.Drawing.Point(55, 128);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Helvetica Neue", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(253, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "İlçe";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(256, 128);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(290, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "string[] meyveler = {\"muz\", \"çilek\", \"elma\",\"ayva\",\"kavun\"};";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ComboBox.İtems.AddRange(meyveler);";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 338);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 24.12.2020_ornek2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24._12._2020_ornek2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            if (comboBox1.SelectedItem == "Antalya")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Kepez");
+                comboBox2.Items.Add("Gazipaşa");
+                comboBox2.Items.Add("Alanya");
+            }
+
+
+            /*
+         
+           _                                                    
+ | |      | |                                            
+ | |_ ___ | | __ _  __ _   _ __   ___   __ _  __ _ _   _ 
+ | __/ _ \| |/ _` |/ _` | | '_ \ / _ \ / _` |/ _` | | | |
+ | || (_) | | (_| | (_| | | | | | (_) | (_| | (_| | |_| |
+  \__\___/|_|\__, |\__,_| |_| |_|\___/ \__, |\__,_|\__, |
+              __/ |                     __/ |       __/ |
+             |___/                     |___/       |___/ 
+        𝐭𝐨𝐥𝐠𝐚𝐧𝐨𝐠𝐚𝐲.𝐜𝐨𝐦 / 𝐓𝐨𝐥𝐠𝐚 N𝐨𝐠𝐚𝐲
+         
+             */
+
+            else if (comboBox1.SelectedItem == "Isparta")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Senirkent");
+                comboBox2.Items.Add("Uluborlu");
+                comboBox2.Items.Add("Keçiborlu");
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+      
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
